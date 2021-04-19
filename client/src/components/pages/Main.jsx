5a4080854d3a2f4bf8fd988dbd8c13dc97c63323
jaycammarano <jaycammarano@gmail.com>
2021-04-18 21:09:00 -0400

    split styles

--- client/src/components/pages/Main.jsx
+++ client/src/components/pages/Main.jsx
@@ -5,16 +5,14 @@ import "../../assets/css/style.css"
 const Main = () => {
     return (
         <div class="wrapper">
-                <div class="inner">
-                    <div >Weather</div>
-                    <div>Forecast</div>
-                </div>
-            	<div id="wizard" role="application" class="wizard clearfix">
-                    <div class="content clearfix">
-	                    <h4 id="wizard-h-0" tabindex="-1" class="title current">Choose Date</h4>
+            <div class="inner">
+                <div >Weather</div>
+                <div>Forecast</div>
+            </div>
+            <div id="wizard" role="application" class="wizard clearfix">
+                <div class="content clearfix">
                     <Info />
-
-            	</div>
+                </div>
             </div>
 		</div>
     )
