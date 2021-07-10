ef4ecda40022032b59fe4ce623fb84369418a954
jaycammarano <jaycammarano@gmail.com>
2021-07-10 18:53:38 -0400

    fixed a warning

--- client/src/components/components/LocationInput.jsx
+++ client/src/components/components/LocationInput.jsx
@@ -21,6 +21,7 @@ const LocationInput = ({ setLocation }) => {
 
   useEffect(() => {
     setLocation(data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
   const onChange = async (e) => {
